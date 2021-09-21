7ab528d64bc52dea9ee38e7eea9a2286ce2adab0
adrien-dubois <mcsnoos@gmail.com>
2021-09-21 14:28:15 +0200

    Announce controller

---
+++ src/Controller/Api/V1/AnnounceController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Api\V1;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AnnounceController extends AbstractController
+{
+    /**
+     * @Route("/api/v1/announce", name="api_v1_announce")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/Api/V1/AnnounceController.php',
+        ]);
+    }
+}
