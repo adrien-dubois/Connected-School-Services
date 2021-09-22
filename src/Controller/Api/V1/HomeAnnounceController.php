aa3bd797260b54c2660e433b7a0714d74ded06d0
adrien-dubois <mcsnoos@gmail.com>
2021-09-22 16:04:31 +0200

    Add a public endpoint for announce by ID

--- src/Controller/Api/V1/HomeAnnounceController.php
+++ src/Controller/Api/V1/HomeAnnounceController.php
@@ -7,16 +7,47 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/api/v1/homeannounce", name="api_v1_home_announce_", requirements={"id"="\d+"})
+ */
 class HomeAnnounceController extends AbstractController
 {
     /**
-     * @Route("/api/v1/homeannounce", name="api_v1_home_announce", methods={"GET"})
+     * @Route("/", name="index", methods={"GET"})
+     *
+     * @param AnnounceRepository $announceRepository
+     * @return Response
      */
     public function index(AnnounceRepository $announceRepository): Response
     {
         $announces = $announceRepository->findAll();
 
         return $this->json($announces, 200, [],[
+            'groups' => 'announce'
+        ]);
+    }
+
+    /**
+     * Get an announce by its ID
+     * 
+     * @Route("/{id}", name="show", methods={"GET"})
+     *
+     * @param integer $id
+     * @param AnnounceRepository $announceRepository
+     * @return void
+     */
+    public function show(int $id, AnnounceRepository $announceRepository)
+    {
+        $announce = $announceRepository->find($id);
+
+        if(!$announce){
+            return $this->json([
+                'error' => 'L\'annonce numéro ' . $id . ' n\'existe pas.'
+            ], 404
+            );
+        }
+
+        return $this->json($announce, 200, [], [
             'groups' => 'announce'
         ]);
     }
