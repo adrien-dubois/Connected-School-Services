d80d23ef517f7f2fb3c29431f55e19d2654f5223
adrien-dubois <mcsnoos@gmail.com>
2021-09-26 22:59:47 +0200

    Change homework String to Text

--- src/Entity/Announce.php
+++ src/Entity/Announce.php
@@ -56,7 +56,7 @@ class Announce
     private $image;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="text", nullable=true)
      * @Groups({"announce"})
      */
     private $homework;
