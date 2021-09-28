983977a117a9f9a1fa43fd8c83fc5d9646ff2a6f
adrien-dubois <mcsnoos@gmail.com>
2021-09-28 18:07:15 +0200

    Edit uploader for Put & Patch

--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -2,19 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\DisciplineRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/teacher", name="teacher_", requirements={"id"="\d+"})
+ */
 class TeacherController extends AbstractController
 {
     /**
-     * @Route("/teacher", name="teacher")
+     * Display all discipline to order teachers
+     *
+     * @Route("/", name="home", methods={"GET"})
+     * 
+     * @return Response
      */
-    public function index(): Response
+    public function index(DisciplineRepository $discipline): Response
     {
-        return $this->render('teacher/index.html.twig', [
-            'controller_name' => 'TeacherController',
+        return $this->render('teacher/teacher.html.twig', [
+                'disciplines'=>$discipline->findAll()
         ]);
     }
+
+
 }
